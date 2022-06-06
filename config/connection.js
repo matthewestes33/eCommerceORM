0a7c4a1f8b9bbb7da285f7dd8f538f1e68ebb966
Matt Estes <me6384@gmail.com>
2022-06-06 10:56:26 -0700

    enable sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+//may need to change this fit week 13 models
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -9,6 +10,8 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
+      // added port
+      port: 3306,
       },
     });
 
