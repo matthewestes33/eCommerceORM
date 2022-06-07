e788d43b78be679c0678239721c2aef98faaaa76
Matt Estes <me6384@gmail.com>
2022-06-07 14:31:32 -0700

    clear old comment

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-//may need to change this fit week 13 models
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
